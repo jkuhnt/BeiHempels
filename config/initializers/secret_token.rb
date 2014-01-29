4e9422fee5a670512f7b633ce1b1d4df0541b500
Judith Kuhnt <jku@tridoco.com>
2014-01-29 13:05:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeiHempels::Application.config.secret_key_base = '5afce0b5dc520bdfd2aed0165c21c71752db66f098c67368bb3f48b52ebc5e8fb2f67c541fb8f8855c98721aadd15341d2dfe5bc2a23afcbecb59cf4d1451253'
